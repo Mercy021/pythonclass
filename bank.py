bb32ffad000db3221b0a76c0ab800e8d9c0a771a
mercy021 <kinyanjuimercywangari@gmail.com>
2021-05-31 22:42:51 +0300

    first commit

--- bank.py
+++ bank.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 class Bank:
     
-    # type="online"
+    
      name="kcb"
     
     
@@ -8,27 +9,56 @@ class Bank:
          self.phonenumber=phonenumber
          self.name=name
          self.balance=0
-
+         self.statement=[]
+         
      def show_balance(self):
           return f"Hello{self.name}your balance is{self.balance}" 
+      
      def deposit(self,amount):
-         self.balance+=amount
-         return self.show_balance()
-     def withdraw(self,amount):
-        self.balance-=amount
+        if amount<0:
+            return "refused" 
+        else:
+            self.balance+=amount 
+            now=datetime.now()
+            transaction={
+               "amount":amount,
+               "time":now,
+               "narration":"you deposited"
+               }
+            self .statement.append(transaction)
+            
         return self.show_balance()
-     def update(self,amount):
+    
+     def withdraw(self,amount):
          if amount>0:
-            self.balance+=amount
-            return f"Hello your balance is{self.show_balance}"
-         else:
-          return f"you cannot deposit{amount}it is beyond minimum"
+          self.balance-=amount
+          now=datetime.now()
+          transaction={
+               "amount":amount,
+               "time":now,
+               "narration":"you deposited"
+               }
+         self .statement.append(transaction)
+         return self.show_balance()
+    
      def borrow(self,amount):
          self.balance+=amount
          return f"Dear customer{self.name} you have borrowed{self.loan}"
+     
      def repay(self,amount):
          self.loan-=amount
          return f"Dear customer{self.name} you loan is{self.loan} shillings you have paid{amount}"
+     def show_statement(self):
+        for transaction in self.statement:
+             amount=transaction["amount"]
+             narration=transaction["narration"]
+             time=transaction["time"]
+             date=time.strftime("%d/%m/%y")
+             print (f"{date}:{narration} {amount}")
+        
+        
+             
+         
       
          
          
